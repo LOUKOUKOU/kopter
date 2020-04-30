72ca9f3fa7d61ef7fbe38bd79b24914a848aed33
Louw Hyman <louw@gtconsult.com>
2020-04-30 21:28:56 +0200

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Inside vue.config.js
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+        manifestOptions: {
+            orientation: "landscape",
+            display: "fullscreen",
+            background_color: "#F1AD2D"
+        }
+    }
+}
